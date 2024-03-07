6a12b6fb1c32a8a3c6b77245f637953fd24951c7
thiagolucca167 <thiagoluccarodrigues167@gmail.com>
2024-03-07 13:29:07 -0300

    atualização header, menu e icones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
     var menuList = document.querySelector('.menu-list');
-    menuList.style.display = (menuList.style.display === 'none' || menuList.style.display === '') ? 'block' : 'none';
+    menuList.style.display = (menuList.style.display === 'none' || menuList.style.display === '') ? 'flex' : 'none';
+    menuList.style.flex-direction( menuList.style.flex-direction === '') ? 'column': '';
+    
 }
