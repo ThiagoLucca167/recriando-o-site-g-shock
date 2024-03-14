7297727924437e55a65099c72d10a711f18ecf26
thiagolucca167 <thiagoluccarodrigues167@gmail.com>
2024-03-14 12:41:56 -0300

    atualização banner, responsivo e adequação do viewport de 100vh menu e banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 function toggleMenu() {
     var menuList = document.querySelector('.menu-list');
     menuList.style.display = (menuList.style.display === 'none' || menuList.style.display === '') ? 'flex' : 'none';
-    menuList.style.flex-direction( menuList.style.flex-direction === '') ? 'column': '';
-    
+    menuList.style.flexDirection = (menuList.style.display === 'none' || menuList.style.display === '') ? 'column' : ''; // Corrigindo a atribuição da direção do flexbox
 }
